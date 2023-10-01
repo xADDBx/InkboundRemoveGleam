7e2f9c554fa4f14c8a3f8160f3b6bdf597bd51a3
ADDB <addb1234567890@gmail.com>
2023-10-01 15:32:03 +0200

    w

--- RemoveGleam.cs
+++ RemoveGleam.cs
@@ -44,4 +44,9 @@ namespace RemoveGleam {
             }
         }
     }
+    public static class PluginInfo {
+        public const string PLUGIN_GUID = "RemoveGleam";
+        public const string PLUGIN_NAME = "RemoveGleam";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
 }
